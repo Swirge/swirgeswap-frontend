2d9f4c87922f754e588cbe91cc5a910af7fd66a7
preshnwoko <preshnwoko@gmail.com>
2020-09-21 12:40:31 +0100

    removing the chef logo and adding swirge logo

--- src/views/Farm/components/Stake.tsx
+++ src/views/Farm/components/Stake.tsx
@@ -20,6 +20,7 @@ import useUnstake from '../../../hooks/useUnstake'
 import { getBalanceNumber } from '../../../utils/formatBalance'
 import DepositModal from './DepositModal'
 import WithdrawModal from './WithdrawModal'
+import swgicon from "../../../assets/img/Swirge.png"
 
 interface StakeProps {
   lpContract: Contract
@@ -73,7 +74,9 @@ const Stake: React.FC<StakeProps> = ({ lpContract, pid, tokenName }) => {
       <CardContent>
         <StyledCardContentInner>
           <StyledCardHeader>
-            <CardIcon>👨🏻‍🍳</CardIcon>
+            <CardIcon>
+              <img src ={swgicon} alt = "SWG" width = "100%" />
+            </CardIcon>
             <Value value={getBalanceNumber(stakedBalance)} />
             <Label text={`${tokenName} Tokens Staked`} />
           </StyledCardHeader>
